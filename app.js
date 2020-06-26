4b495a1f8f70930e4ed1e3ea27810de8698f4e71
renanvzd <renanvz@outlook.com>
2020-06-26 20:50:56 -0300

    Estrutura Estática

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
 // using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
 // app.use("/route", express.static("foldername"));
-app.use('/public', express.static('public'));
+app.use('/public', express.static('public'))
 
 app.get('/', function(req, res) {
     res.render('home-guest')
