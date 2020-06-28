8356a581d35351add8d6e900168868c2cedec887
renanvzd <renanvz@outlook.com>
2020-06-28 16:31:16 -0300

    Heroku

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express()
 
 const router = require('./router')
 
+
+let port = process.env.PORT
+if (port == null || port == "") {
+    port=3000
+}
+
 app.use('/public', express.static('public'))
 app.use('/models', express.static('models'))
 app.set('views', 'views')
@@ -10,9 +16,7 @@ app.set('view engine', 'ejs')
 
 app.use('/', router)
 
-let port = process.env.PORT
-if (port == null || port == "") {
-    port=3000
-}
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.listen(port)
