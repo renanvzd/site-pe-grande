502b8f556fe7d1633000bd01af14f9de11fb638b
renanvzd <renanvz@outlook.com>
2020-06-28 15:54:38 -0300

    Integracao com Javascript

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const router = require('./router')
 
 app.use('/public', express.static('public'))
+app.use('/models', express.static('models'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
