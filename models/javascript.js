0f8aa5d20b1fc78a2490feb6d590f138ea7b44eb
renanvzd <renanvz@outlook.com>
2020-07-03 21:52:51 -0300

    Menu Scroll

--- models/javascript.js
+++ models/javascript.js
@@ -71,10 +71,22 @@ var vload;
 
 
         if($(window).scrollTop() > 0) {
-            $(".headerquem").addClass("sticky");
+            $(".menu-tab").addClass("sticky");
         } else {
-            $(".headerquem").removeClass("sticky");
+            $(".menu-tab").removeClass("sticky");
         }
 
     });
 });
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
